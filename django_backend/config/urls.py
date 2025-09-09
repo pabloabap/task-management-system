e3df865bf220c8974f444d2bdfa442bb0214ae1e
Pablo Abad Aparicio <pablo.abad96@gmail.com>
2025-09-09 23:20:15 +0200

    Clean up urls.py by removing unused code
    Removed commented-out code for user serialization and viewsets.

--- django_backend/config/urls.py
+++ django_backend/config/urls.py
@@ -3,31 +3,10 @@ from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 from . import settings
 from django.contrib.auth import get_user_model
-
-# Serializers define the API representation.
-#class UserSerializer(serializers.HyperlinkedModelSerializer):
-#    class Meta:
-#        model = get_user_model()
-#        fields = ['url', 'username', 'email', 'is_staff']
-#
-## ViewSets define the view behavior.
-#class UserViewSet(viewsets.ModelViewSet):
-#    queryset = get_user_model().objects.all()
-#    serializer_class = UserSerializer
-#
-## Routers provide an easy way of automatically determining the URL conf.
-#router = routers.DefaultRouter()
-#router.register(r'users', UserViewSet)
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('apps.tasks.urls')),
     path('users/', include('apps.users.urls')),
     path('api/', include('apps.api.urls')),
 	path("api/auth/", include('rest_framework.urls')),
-
-#   path('api/', include('rest_framework.urls', namespace='rest_framework')),	
-#   path('api/', include(router.urls)),
-#  	path("auth/", include('rest_framework.urls')),
-
 ]
